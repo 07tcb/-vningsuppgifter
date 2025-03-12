019b4fb2680a3a30a0182d94e02a6e4a79b5e181
07tcb <tinder.carlesonbjorn@skola.taby.se>
2025-03-12 23:10:47 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+const people = [
+    { name: "Alice", age: 30 },
+    { name: "Bob", age: 25 },
+    { name: "Charlie", age: 20 },
+    { name: "David", age: 35 },
+    { name: "Emma", age: 28 },
+    { name: "Frank", age: 42 },
+    { name: "Grace", age: 19 }
+  ];
+  
+people.forEach(shurda => {
+    console.log("dricka kiss är gott")
+});
